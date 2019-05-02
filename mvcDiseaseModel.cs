c76dfd6a55b1702df3df727c70b510b88b3ec90f
Emannuel625 <47412692+Emannuel625@users.noreply.github.com>
2019-05-02 10:46:05 +0200

    Add files via upload

---
+++ mvcDiseaseModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc.Models
+{
+    public class mvcDiseaseModel
+    {
+
+
+        
+
+
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public mvcDiseaseModel()
+        {
+            this.Disease_Prevention = new HashSet<mvcDisease_PreventionModel>();
+        }
+
+        public int Disease_ID { get; set; }
+
+        [Required(ErrorMessage = "This Field is required")]
+        public string Disease_Name { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<mvcDisease_PreventionModel> Disease_Prevention { get; set; }
+
+    }
+}
---
+++ mvcDisease_PreventionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Mvc.Models
+{
+    public class mvcDisease_PreventionModel
+    {
+        public int Prevention_ID { get; set; }
+
+        [Required(ErrorMessage = "This Field is required")]
+        public string Prevention_Description { get; set; }
+        public Nullable<int> Disease_ID { get; set; }
+
+        public virtual mvcDiseaseModel Disease { get; set; }
+
+    }
+}
